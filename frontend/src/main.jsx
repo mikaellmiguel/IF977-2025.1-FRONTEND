96c662eca47489db50c8d62d29b02ae984ba10b6
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-07-14 22:20:52 -0300

    feat: substitui o componente App pela Homepage e adiciona ToastContainer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { Homepage } from './pages/Homepage/index.jsx'
 import { GlobalStyle } from './styles/global.js'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GlobalStyle />
-    <App />
+    <ToastContainer position='top-center' toastStyle={{ width: "35rem", fontSize: "1rem", padding: "1.5rem", fontFamily: "Poppins"}}/>
+    <Homepage />
   </StrictMode>,
 )
