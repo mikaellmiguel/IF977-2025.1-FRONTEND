1f87974037e11144fd46c5e772d944de54b5d59b
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-07-14 22:18:27 -0300

    fix: corrige a referência da imagem e atualiza o estado exibido no componente DeputadoCard

--- frontend/src/components/DeputadoCard/index.jsx
+++ frontend/src/components/DeputadoCard/index.jsx
@@ -5,11 +5,11 @@ import { Card, CardData, CardImage} from "./styles"
 export function DeputadoCard({ deputado }) {
   return (
     <Card>
-        <CardImage src={deputado.url} alt={`${deputado.nome} - ${deputado.partido}`} />
+        <CardImage src={deputado.url_foto} alt={`${deputado.nome} - ${deputado.partido}`} />
         <CardData>
             <h2>{deputado.nome}</h2>
             <p><strong>Partido: </strong>{deputado.partido}</p>
-            <p><strong>Estado:</strong> {deputado.estado}</p>
+            <p><strong>Estado:</strong> {deputado.sigla_uf}</p>
             <p><strong>Nascimento:</strong> {formatarDataBR(deputado.data_nascimento)} ({calcularIdade(deputado.data_nascimento)})</p>
         </CardData>
     </Card>
