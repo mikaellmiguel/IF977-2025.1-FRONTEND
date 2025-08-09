293b2af518d5fd904c958167e9c77ae90ff5df55
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-08-09 12:28:22 -0300

    feat: integra tipos e estados no hook useDeputadoDetails

--- frontend/src/pages/DeputadosDetails/index.jsx
+++ frontend/src/pages/DeputadosDetails/index.jsx
@@ -8,19 +8,7 @@ import { DeputadoDespesas } from "./DeputadoDespesas";
 export function DeputadosDetails() {
   const { id } = useParams();
   const navigate = useNavigate();
-  const { deputado, despesas, totalPages, page, setPage, filtros, setFiltros } = useDeputadoDetais(id);
-
-  // Tipos e estados mockados
-  const tipos = [
-	"AQUISIÇÃO DE TOKENS E CERTIFICADOS DIGITAIS",
-	"ASSINATURA DE PUBLICAÇÕES",
-	"COMBUSTÍVEIS E LUBRIFICANTES.",
-	"CONSULTORIAS, PESQUISAS E TRABALHOS TÉCNICOS.",
-	"DIVULGAÇÃO DA ATIVIDADE PARLAMENTAR.",
-	"FORNECIMENTO DE ALIMENTAÇÃO DO PARLAMENTAR",
-	"HOSPEDAGEM ,EXCETO DO PARLAMENTAR NO DISTRITO FEDERAL."
-  ];
-  const estados = ["PE", "SP", "RJ", "DF", "MG", "RS", "BA", "PR", "SC", "GO", "ES", "CE", "PA", "AM", "MA", "PB", "RN", "PI", "MT", "MS", "SE", "AL", "RO", "TO", "AC", "AP", "RR"];
+  const { deputado, despesas, totalPages, page, setPage, filtros, setFiltros, tipos, estados } = useDeputadoDetais(id);
 
   return (
 	<Container>
@@ -28,14 +16,14 @@ export function DeputadosDetails() {
 	  <MainContent>
 		<DeputadoContato deputado={deputado} />
 		<DeputadoDespesas
-		  tipos={tipos}
-		  estados={estados}
-		  despesas={despesas}
-		  totalPages={totalPages}
-		  page={page}
-		  setPage={setPage}
-		  filtros={filtros}
-		  setFiltros={setFiltros}
+			tipos={tipos}
+			estados={estados}
+			despesas={despesas}
+			totalPages={totalPages}
+			page={page}
+			setPage={setPage}
+			filtros={filtros}
+			setFiltros={setFiltros}
 		/>
 	  </MainContent>
 	</Container>
