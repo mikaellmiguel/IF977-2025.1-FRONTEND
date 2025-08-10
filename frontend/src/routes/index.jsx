c5bbf353e87338c9fd439a8d0885257d1fedcca0
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-08-09 23:30:17 -0300

    fix: adiciona rotas para página 404 nas rotas de autenticação e aplicação

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -2,7 +2,7 @@ import {BrowserRouter} from "react-router-dom";
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
 
-import { useAuth } from "../hooks/auth";
+import { useAuth } from "../hooks/useAuth";
 
 export function Routes() {
     const { user } = useAuth();
