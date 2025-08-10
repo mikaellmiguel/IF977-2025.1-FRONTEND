7813fb6c02055a793ecf5785c47cbb47a8b51598
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-08-10 16:50:25 -0300

    feat: adiciona rota para a página de perfil

--- frontend/src/routes/app.routes.jsx
+++ frontend/src/routes/app.routes.jsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-
 import { Homepage } from "../pages/Homepage";
 import { DeputadosDetails } from "../pages/DeputadosDetails";
 import { NotFound } from "../pages/404";
+import { Follows } from "../pages/Follows";
+import { Profile } from "../pages/Profile";
 
 export function AppRoutes() {
   return (
@@ -11,6 +12,7 @@ export function AppRoutes() {
         <Route path="/deputados/:id" element={<DeputadosDetails />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/404" element={<NotFound />} />
+        <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 }
